d68857768525cceeab0656c5abd562e8d4e61a6d
Roman Roibu <roman@roibu.xyz>
2025-11-14 16:18:21 +0100

    Update tests

--- tests/sailfish-api.test.ts
+++ tests/sailfish-api.test.ts
@@ -18,34 +18,45 @@ describe('SailfishApi', () => {
     expect(latestBlock).not.toBeInstanceOf(Error);
     expect(typeof latestBlock).toBe('number');
     const tradesQuery: TradesQuery = {
-      from_block: latestBlock as number - 1_000,
-      to_block: latestBlock as number,
+      lower_tick: latestBlock as number - 1_000,
+      upper_tick: latestBlock as number,
+      pool_types: [],
       pool_addresses: ["3ucNos4NbumPLZNWztqGHNFFgkHeRMBQAVemeeomsUxv"], // USDC/SOL pool
+      token_addresses: [],
+      to_wallets: [],
+      from_wallets: [],
     };
     const trades_result = await api.fetchTrades(tradesQuery);
     expect(trades_result).not.toBeInstanceOf(Error);
-    console.log('Fetch trades:', trades_result["3ucNos4NbumPLZNWztqGHNFFgkHeRMBQAVemeeomsUxv"]?.length);
+    if (trades_result instanceof Error) {
+      expect.fail("sanity check");
+    }
+    console.log('Fetch trades:', trades_result);
   });
 
   it('should return an error for invalid block range', async () => {
     let tradesQuery: TradesQuery = {
-      from_block: 0,
-      to_block: 1000000000,
+      lower_tick: 0,
+      upper_tick: 1000000000,
+      pool_types: [],
       pool_addresses: [],
+      token_addresses: [],
+      to_wallets: [],
+      from_wallets: [],
     };
     const trades = await api.fetchTrades(tradesQuery);
     expect(trades).toBeInstanceOf(Error);
     console.log('Error:', trades?.message);
 
-    tradesQuery.from_block = 1000000000;
-    tradesQuery.to_block = 999999999;
+    tradesQuery.lower_tick = 1000000000;
+    tradesQuery.upper_tick = 999999999;
     const trades2 = await api.fetchTrades(tradesQuery);
     expect(trades2).toBeInstanceOf(Error);
     console.log('Error:', trades2?.message);
 
 
-    tradesQuery.from_block = -1;
-    tradesQuery.to_block = 1000000001;
+    tradesQuery.lower_tick = -1;
+    tradesQuery.upper_tick = 1000000001;
     const trades3 = await api.fetchTrades(tradesQuery);
     expect(trades3).toBeInstanceOf(Error);
     console.log('Error:', trades3?.message);
@@ -67,16 +78,20 @@ describe('SailfishApi', () => {
   it('should fetch graduated pools', async () => {
     const graduated_block = 374388563;
     const graduatedPoolsQuery: GraduatedPoolsQuery = {
-      from_block: graduated_block - 99_990,
-      to_block: graduated_block + 1,
+      lower_tick: graduated_block - 990,
+      upper_tick: graduated_block + 1,
       pool_types: [PoolType.PumpFunAmm, PoolType.RaydiumLaunchpad],
       pool_addresses: [],
+      token_addresses: [],
     };
     const graduatedPools = await api.fetchRawGraduations(graduatedPoolsQuery);
     if (graduatedPools instanceof Error) {
       console.error('Fetch graduated pools Error:', graduatedPools.message);
     }
     expect(graduatedPools).not.toBeInstanceOf(Error);
-    console.log(`Raw graduations - pump: ${graduatedPools?.pumpswap_graduations.length}, raydium_amm: ${graduatedPools?.raydium_amm_graduations.length}, raydium_cpmm: ${graduatedPools?.raydium_cpmm_graduations.length}`);
+    if (graduatedPools instanceof Error) {
+      expect.fail("sanity check");
+    }
+    console.log(`Raw graduations - pump: ${graduatedPools.pumpswap_graduations.length}, raydium_amm: ${graduatedPools.raydium_amm_graduations.length}, raydium_cpmm: ${graduatedPools.raydium_cpmm_graduations.length}`);
   }, 5 * 60 * 1000); // 5 minutes timeout
 });
